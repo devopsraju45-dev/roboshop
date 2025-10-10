968a0c0bdbe66ac903f3cc84ce4030bce4dae44e
prudhviratnakaram <devopsraju45@gmail.com>
2025-10-10 18:50:33 -0500

    Add componenet scripts

--- user.sh
+++ user.sh
@@ -1,5 +1,5 @@
  set -e
- curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
+ curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>>/tmp/user.log
  yum install nodejs -y
  useradd roboshop
  curl -s -L -o /tmp/user.zip "https://github.com/roboshop-devops-project/user/archive/main.zip"
