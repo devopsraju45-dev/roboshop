38351fa5724dc69498bc853474cdbdbb2f688adf
prudhviratnakaram <devopsraju45@gmail.com>
2025-10-11 10:34:22 -0500

    Add componenet scripts

--- cart.sh
+++ cart.sh
@@ -2,41 +2,77 @@
 
  echo setting nodejs
  curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash -
- echo $?
+ if [ $? -eq 0 ]; then
+   echo -e "\esuccess"
+ else
+   echo -e "failure"
+ fi
 
  echo install nodejs
  yum install nodejs -y
- echo $?
+  if [ $? -eq 0 ]; then
+    echo -e "\esuccess"
+  else
+    echo -e "failure"
+  fi
 
  echo adding user
  useradd roboshop
-  echo $?
+   if [ $? -eq 0 ]; then
+     echo -e "\esuccess"
+   else
+     echo -e "failure"
+   fi
 
  echo downloading application content
  curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
  cd /home/roboshop
-  echo $?
+ if [ $? -eq 0 ]; then
+   echo -e "\esuccess"
+ else
+   echo -e "failure"
+ fi
 
  echo cleaning old application
  rm -rf cart
-  echo $?
+ if [ $? -eq 0 ]; then
+   echo -e "\esuccess"
+ else
+   echo -e "failure"
+ fi
 
  echo extract application archive
  unzip /tmp/cart.zip
  mv cart-main cart
  cd cart
-  echo $?
+ if [ $? -eq 0 ]; then
+   echo -e "\esuccess"
+ else
+   echo -e "failure"
+ fi
 
  echo installing nodejs
  npm install
-  echo $?
+ if [ $? -eq 0 ]; then
+   echo -e "\esuccess"
+ else
+   echo -e "failure"
+ fi
 
  echo configuring cart
  mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
  systemctl daemon-reload
-  echo $?
+ if [ $? -eq 0 ]; then
+   echo -e "\esuccess"
+ else
+   echo -e "failure"
+ fi
 
  echo starting cart service
  systemctl start cart
  systemctl enable cart
-  echo $?
+  if [ $? -eq 0 ]; then
+    echo -e "\esuccess"
+  else
+    echo -e "failure"
+  fi
