600681e0a323a4cd270fa14ed532a8a036d69d2b
prudhviratnakaram <devopsraju45@gmail.com>
2025-10-12 18:43:40 -0500

    Add componenet scripts

--- mongodb.sh
+++ mongodb.sh
@@ -11,14 +11,16 @@ StatusCheck
  yum install -y mongodb-org
  StatusCheck
 
+ echo Update Listen address
+ sed -i 's/1277.0.0.1/0.0.0.0/' /etc/mongod.conf
+ StatusCheck
+
  echo Start MONGODB service
  systemctl enable mongod
  systemctl start mongod
  systemctl restart mongod
 
- echo Update Listen address
- sed -i 's/1277.0.0.1/0.0.0.0/' /etc/mongod.conf
- StatusCheck
+
 
 
 DOWNLOAD
