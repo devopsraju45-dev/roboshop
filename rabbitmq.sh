1b6e5907d86f0e08f0fc43f456922ab841bd05a1
prudhviratnakaram <devopsraju45@gmail.com>
2025-10-12 14:48:50 -0500

    Add componenet scripts

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,14 +1,23 @@
- set -e
- curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash
+source common.sh
 
+COMPONENT=rabbitmq
+
+echo Setup YUM Repos
+ curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash
+StatusCheck
  yum install erlang -y
 
  curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
 
+echo  Install rabbitmq
  yum install rabbitmq-server -y
+ StstusCheck
 
  systemctl enable rabbitmq-server
  systemctl start rabbitmq-server
+
+ echo Add tags to rabbitmq
+
 
  rabbitmqctl add_user roboshop roboshop123
  rabbitmqctl set_user_tags roboshop administrator
