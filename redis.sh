bccb5940d72bb4f2eca424b7eed13f90a22e70bf
prudhviratnakaram <devopsraju45@gmail.com>
2025-10-12 12:24:46 -0500

    Add componenet scripts

--- redis.sh
+++ redis.sh
@@ -1,5 +1,15 @@
- set -e
+source common.sh
+
+COMPONENT=redis
+
+echo setup YUM Repo
  curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+ StatusCheck
+
+ echo install Redis
  yum install redis-6.2.13 -y
- systemctl enable redis
- systemctl start redis
+ StatusCheck
+
+ echo Start Redis service
+ systemctl enable redis && systemctl start redis
+ StatusCheck
