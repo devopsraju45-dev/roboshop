0d7a74f95655322d22ee566043fe9a02b18101ea
prudhviratnakaram <devopsraju45@gmail.com>
2025-10-12 14:53:36 -0500

    Add componenet scripts

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -2,6 +2,10 @@ source common.sh
 
 COMPONENT=rabbitmq
 
+if [ -Z "$APP_RABBITMQ_PASSWORD" ]; then
+  echo -e "env var rabbitmq password is needed"
+  exit 1
+fi
 echo Setup YUM Repos
  curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash
 StatusCheck
@@ -19,6 +23,6 @@ echo  Install rabbitmq
  echo Add tags to rabbitmq
 
 
- rabbitmqctl add_user roboshop roboshop123
+ rabbitmqctl add_user roboshop ${APP_RABBITMQ_PASSWORD}
  rabbitmqctl set_user_tags roboshop administrator
  rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
