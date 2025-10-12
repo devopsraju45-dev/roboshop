0216a8ebee382c96cbe67fcdd1ab006c89d46a43
prudhviratnakaram <devopsraju45@gmail.com>
2025-10-12 12:35:59 -0500

    Add componenet scripts

--- mysql.sh
+++ mysql.sh
@@ -1,11 +1,19 @@
- set -e
+source common.sh
 
+COMPONENT=mongodb
+
+echo SETUP YUM REPO
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
+StatusCheck
+
+echo Start Mysql
 dnf module disable mysql
  yum install mysql-community-server -y
 
  systemctl enable mysqld
  systemctl start mysqld
+
+ StatusCheck
 
  grep temp /var/log/mysqld.log
 
@@ -14,6 +22,8 @@ dnf module disable mysql
  DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
 
  echo "alter user 'root'@'localhost' identified with mysql_native_password by 'RoboShop@1';" | mysql -uroot -p${DEFAULT_PASSWORD}
+
+ exit
 
  mysql -uroot -pRoboShop@1
 
